8593b9b6721a843096edef3e5056ca66c27e3fd5
harmeetsingh1 <76486470+harmeetsingh1@users.noreply.github.com>
2023-11-14 17:46:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.DB_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
